7e161a1c7f3d1439bfffa9cd798a261430a9a92e
marcuwynu23 <marcuwynu23@gmail.com>
2022-10-22 10:12:53 +0800

    fix: change index.html content

--- camarinessur.js
+++ camarinessur.js
@@ -1,4 +1,19 @@
 
+
+
+function getAllCityByProvinceCodeList(callback) {
+	fetchData("https://ph-address.onrender.com/ph/citymun?regCode=05&provCode=0517", (data) => {
+		callback(data)
+	})
+}
+
+function getAllBarangayByCityCodeList(code, callback) {
+	fetchData("https://ph-address.onrender.com/ph/barangays?provCode=0517&citymunCode=" + `${code}`, (data) => {
+		callback(data)
+	})
+}
+
+
 function getAllCityByProvinceCode(callback) {
 	fetchData("https://ph-address.onrender.com/ph/citymun?regCode=05&provCode=0517", (data) => {
 		for (var d of data) {
@@ -7,7 +22,15 @@ function getAllCityByProvinceCode(callback) {
 	})
 }
 function getAllBarangayByCityCode(code, callback) {
-	fetchData("https://ph-address.onrender.com/ph/barangays?provCode=0517&citymunCode=" + code, (data) => {
+	fetchData("https://ph-address.onrender.com/ph/barangays?provCode=0517&citymunCode=" + `${code}`, (data) => {
+		for (var d of data) {
+			callback(d)
+		}
+	})
+}
+
+function getAllBarangayByCityCode(code, callback) {
+	fetchData("https://ph-address.onrender.com/ph/barangays?provCode=0517&citymunCode=" + `${code}`, (data) => {
 		for (var d of data) {
 			callback(d)
 		}
